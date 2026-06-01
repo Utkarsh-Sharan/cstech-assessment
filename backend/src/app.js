eeb730e0c8d8049ae32cb5976838c99e43fd23ef
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2026-06-01 15:24:24 +0530

    create agent model

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,7 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({limit: "16kb"}));
 app.use(cookieParser());
 
 import authRoutes from "./routes/auth.routes.js";
---
+++ backend/src/models/agent.model.js
@@ -0,0 +1,24 @@
+import mongoose, {Schema} from "mongoose";
+
+const agentSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+});
+
+export const Agent = mongoose.model("Agent", agentSchema);
