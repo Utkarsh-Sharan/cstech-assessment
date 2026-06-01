5c7370d399a9d369ae03952543635491eeedd78a
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2026-06-01 15:05:01 +0530

    create login user route and controller

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -35,10 +35,47 @@ const registerUser = async (req, res) => {
             }
         });
     } catch (error) {
-        console.error("Failed to register user!", error);
+        return res.status(400).json({message: "Failed to register user!", error});
+    }
+}
+
+const loginUser = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        
+        const user = await User.findOne({email});
+
+        if(!user) return res.status(401).json({message: "Unauthorized access!"});
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if(!isPasswordValid) return res.status(401).json({message: "Incorrect password!"});
+
+        const token = jwt.sign({
+            id: user._id,
+            email: user.email
+        }, process.env.JWT_SECRET);
+
+        res.cookie("token", token);
+
+        return res.status(200).json({
+            message: "User logged in successfully!",
+            user: {
+                id: user._id,
+                fullName: user.fullName,
+                email: user.email,
+            }
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(400).json({
+            message: "Failed to login",
+            error: error
+        });
     }
 }
 
 export {
     registerUser,
+    loginUser,
 }
