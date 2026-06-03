613d49deb5db74293205cca8fe023a4926730384
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2026-06-03 10:15:00 +0530

    setup cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,19 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json({limit: "16kb"}));
 app.use(cookieParser());
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-type", "Authorization"],
+    })
+);
 
 import authRoutes from "./routes/auth.routes.js";
 
